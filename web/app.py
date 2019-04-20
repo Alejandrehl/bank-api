bb25abf63b2307fed86e79e2a7aad38b12d77930
Alejandro <alejandrehl@icloud.com>
2019-04-20 18:30:33 -0400

    Latest steps

--- web/app.py
+++ web/app.py
@@ -222,6 +222,16 @@ class PayLoan(Resource):
         updateDebt(username, debt - money)
 
         return jsonify(generateReturnDictionary(200, "You've succesfully paid your loan."))
+
+api.add_resource(Register, '/register')
+api.add_resource(Add, '/add')
+api.add_resource(Transfer, '/transfer')
+api.add_resource(Balance, '/balancd')
+api.add_resource(TakeLoan, 'takeloan')
+api.add_resource(PayLoan, 'payloan')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
         
 
 
